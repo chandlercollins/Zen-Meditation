79110ccafe3b02fb5a603fe1cbbe2a0269b304cd
Chandler Collins <chanman379@gmail.com>
2024-02-25 16:17:22 -0500

    Initial Commit

---
+++ Zen Meditation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Zen Meditation
+//
+//  Created by Chandler Collins on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zen Meditation/Zen_MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zen_MeditationApp.swift
+//  Zen Meditation
+//
+//  Created by Chandler Collins on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Zen_MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
